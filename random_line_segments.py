d371d91758c96ba5e3489097f3f38686e98832c2
Mee Seong Im <MeeSeongIm@users.noreply.github.com>
2014-10-26 23:19:14 -0500

    Update random_line_segments.py

--- random_line_segments.py
+++ random_line_segments.py
@@ -1,17 +1,17 @@
 
-# prints 20 random line segements 
+# prints i random line segements 
 
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = []
-for _ in xrange(20):
-    data.append((np.random.rand(), np.random.rand()))
-    data.append((np.random.rand(), np.random.rand()))
-    data.append("r")
+i=20
+data = [] 
+for _ in xrange(i):
+    data.append((np.random.rand(), np.random.rand()))  # x1, x2 
+    data.append((np.random.rand(), np.random.rand()))  # y1, y2    
+    data.append("r")                                   # the line segment connecting (x1,y1) and (x2,y2) 
 print("Now plotting: \n")
 plt.plot(*data)
-print("Printed 20 random line segments in a unit square.") 
+print("Printed %s random line segments in a unit square." % i) 
 
-
-
+  
